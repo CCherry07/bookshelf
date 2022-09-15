193ab6fc5d2b176e1b0eced6f9513552a184fa9a
CCherry07 <2405693142@qq.com>
2022-09-15 11:50:43 +0800

    chore:  button style component with other

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@
 import React, { useState } from "react"
 import "@reach/dialog/styles.css"
 import { Logo } from './components/logo'
-import { Button, FormGroup, Input, Dialog } from "./components/lib"
+import { Button, FormGroup, Input, Dialog, Spinner, CircleButton } from "./components/lib"
 import { jsx, css } from "@emotion/react"
+import VisuallyHidden from "@reach/visually-hidden"
 type OpenModal = "none" | "login" | "register"
 type FormData = { username: string, password: string }
 interface LoginFormProps {
+  title: string
   onSubmit: ({ username, password }: FormData) => void
   submitButton: any
 }
 function LoginForm({ onSubmit, ...props }: LoginFormProps) {
-  const { submitButton } = props
+  const { submitButton, title } = props
+  const [isLoading,] = useState(true)
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault()
     const { username, password } = (event.target as any).elements
@@ -21,19 +24,40 @@ function LoginForm({ onSubmit, ...props }: LoginFormProps) {
     })
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <FormGroup>
-        <label htmlFor="username"> Username </label>
-        <Input id="username" type="text" />
-      </FormGroup>
-      <FormGroup>
-        <label htmlFor="password"> Password </label>
-        <Input id="password" type="password" />
-      </FormGroup>
-      <div>
-        {React.cloneElement(submitButton, { type: "submit" })}
+    <>
+      <div css={css`display:flex;justify-content:flex-end`}>
+        <CircleButton>
+          <VisuallyHidden>Close</VisuallyHidden>
+          <span aria-hidden>×</span>
+        </CircleButton>
       </div>
-    </form>
+      <h1 style={{ textAlign: "center" }}>{title}</h1>
+      <form onSubmit={handleSubmit} css={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+        '> div': {
+          margin: '10px auto',
+          width: '100%',
+          maxWidth: '300px',
+        },
+      }}>
+        <FormGroup>
+          <label htmlFor="username"> Username </label>
+          <Input id="username" type="text" />
+        </FormGroup>
+        <FormGroup>
+          <label htmlFor="password"> Password </label>
+          <Input id="password" type="password" />
+        </FormGroup>
+        <div>
+          {React.cloneElement(submitButton, undefined,
+            ...(Array.isArray(submitButton.props.children) ?
+              submitButton.props.children : [submitButton.props.children]),
+            isLoading ? <Spinner css={{marginLeft: 5}} /> : null)}
+        </div>
+      </form>
+    </>
   )
 }
 
@@ -61,16 +85,17 @@ function App() {
       `}>
         <Logo width='80' height='80' />
         <h1> Bookshelf </h1>
-        <div>
-          <button style={{ margin: " 0 20px 0 0", }} onClick={() => setOpenModal("login")}>Login</button>
-          <button onClick={() => setOpenModal("register")}>Register</button>
+        <div css={css`
+          display:flex;
+        `}>
+          <Button style={{ margin: " 0 20px 0 0", }} onClick={() => setOpenModal("login")}>Login</Button>
+          <Button type={"secondary"} onClick={() => setOpenModal("register")}>Register</Button>
         </div>
         <Dialog aria-label="login form" isOpen={openModal === 'login'}>
-          {/* <button onClick={() => setOpenModal("none")}>check</button> */}
-          <LoginForm submitButton={(<Button>Login</Button>)} onSubmit={login} />
+          <LoginForm title="Login" submitButton={(<Button type={"primary"}>Login</Button>)} onSubmit={login} />
         </Dialog>
         <Dialog aria-label="register form" isOpen={openModal === 'register'}>
-          <LoginForm onSubmit={register} submitButton={(<Button>Register</Button>)} />
+          <LoginForm title="Register" onSubmit={register} submitButton={(<Button type={"secondary"}>Register</Button>)} />
         </Dialog>
       </div>
     </div>
