c95d79449d680000cf9db476638c5c95ab553d44
CCherry07 <2405693142@qq.com>
2022-09-18 23:15:00 +0800

    refer:  `useAsync` hook

--- src/context/auth-provider.tsx
+++ src/context/auth-provider.tsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react"
-import { useAsync } from "../hooks"
+import React, { useContext, useState } from "react"
+import { useAsync } from "../hooks";
+import * as auth from '../utils/auth';
 
 const AuthContext = React.createContext({})
 
 function AuthProvider({ children }: { children: any }) {
-  const user = useState({})
-
-  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>
+  const { } = useAsync()
+  const value = {}
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
 
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) throw new Error("useAuth must be used under auth context provider!")
+  return context
+}
 
 export { AuthProvider }
