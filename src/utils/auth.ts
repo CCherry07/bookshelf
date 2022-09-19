37c233c9961a6e7367c5cce1b34ae5fff38da4ef
CCherry07 <2405693142@qq.com>
2022-09-19 17:01:23 +0800

    feat:  auth module

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,15 +1,25 @@
-type FormData = { username: string, password: string }
+export type FormData = { username: string, password: string }
 const authURL = "https://auth-provider.jk/auth"
+
+export const TOKEN_LOCAL_STORAGE_KEY = "TOKEN_LOCAL_STORAGE_KEY"
+
+function handleTokenStorage(token: string) {
+  window.localStorage.setItem(TOKEN_LOCAL_STORAGE_KEY, token)
+}
+const getToken = async () => window.localStorage.getItem(TOKEN_LOCAL_STORAGE_KEY)
 export const login = (data: FormData) => {
   // TODO useLocalStorage
+  const res = client("login", data)
+  console.log(res);
   return client("login", data)
 }
 export const register = (data: FormData) => {
+  const res = client("register", data)
+  console.log(res);
   return client("register", data)
 }
-export const logout = () => {
-  // todo
-}
+export const logout = () => window.localStorage.removeItem(TOKEN_LOCAL_STORAGE_KEY)
+
 function client(endpoint: string, data: FormData) {
   const config = {
     method: 'POST',
