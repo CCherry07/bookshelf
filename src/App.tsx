8856c591d6f7c709813d0ab8d71d58d2da7729c7
CCherry07 <2405693142@qq.com>
2022-09-19 17:02:49 +0800

    feat:  App slice package with auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { AuthenticatedApp } from "./authenticated-app"
+import { useAuth } from "./context/auth-provider"
+import { UnauthenticatedApp } from "./unauthenticated-app"
+
 function App() {
-  
+  const { user, login, register } = useAuth()
+  return user ? <AuthenticatedApp /> : <UnauthenticatedApp login={login} register={register} />
 }
 
 export default App
